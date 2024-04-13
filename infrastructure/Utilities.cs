6d7c077c6525a78d90beecc65891d9114c30ac09
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-04-13 19:03:05 +0200

    added the utilities

---
+++ infrastructure/Utilities.cs
@@ -0,0 +1,9 @@
+﻿namespace infrastructure;
+
+public class Utilities
+{
+    public static readonly string PGPASS = new(Environment.GetEnvironmentVariable("PGPASS"));
+
+    public static readonly string connectionString = "Server=localhost:5432;Database=CurrencyConverted;User Id=appuser;Password=secret;";
+
+}
