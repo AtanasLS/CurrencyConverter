5b50765fc62801341bbafbe93061ece0479f0a88
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-04-21 18:49:57 +0200

    added test for ConverertCurrency class

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cb39c7c109edef9c8fbdffeda03b05697e2dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798b4f8b9697e841f1990f7a9a49e9c7b4adad41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
