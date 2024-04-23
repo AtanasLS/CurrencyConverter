cfc707d9d945955cd348b50e5db5f77541db579f
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-04-23 21:17:19 +0200

    Changed compose file

--- service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d703967aa20fd74887cd7965025a6144d5f311d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a20f4a168de544db0a566e01c231dd53226f118")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
