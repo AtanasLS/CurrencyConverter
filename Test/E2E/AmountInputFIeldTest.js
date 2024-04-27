9f804f2ac2435ad344f13b3023a89b504bf0705d
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-04-27 22:19:39 +0200

    FIxed secrets

--- Test/E2E/AmountInputFIeldTest.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-
-fixture(`Currency Converter App`)
-    .page('http://213.199.36.5:4201/');
-
-test('Amount input field should be present', async t => {
-    const amountInput = Selector('#amount');
-    await t.expect(amountInput.exists).ok();
-});
--- infrastructure/Utilities.cs
+++ infrastructure/Utilities.cs
@@ -4,7 +4,7 @@ public class Utilities
 {
     public static readonly string PGPASS = new(Environment.GetEnvironmentVariable("PGPASS"));
 
-    public static readonly string devConnectionString = "Server=localhost:5432;Database=CurrencyConverted;User Id=appuser;Password=secret;";
+    public static readonly string devConnectionString = $"Server=localhost:5432;Database=CurrencyConverted;User Id=appuser;Password={PGPASS};";
     public static readonly string connectionString = 
-                                "Server=app-database:5432;Database=CurrencyConverted;User Id=appuser;Password=secret;";
+                                $"Server=app-database:5432;Database=CurrencyConverted;User Id=appuser;Password={PGPASS};";
 }
